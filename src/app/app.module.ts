ca9f351910b7706098dba01795e860f11cf9e9ba
RezaRezaeian <rreza.rezaeian@gmail.com>
2023-11-10 06:58:20 -0500

    create login & start component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { StartComponent } from './start/start.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    StartComponent
   ],
   imports: [
     BrowserModule,
