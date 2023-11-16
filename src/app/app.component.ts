4b3461bd8ecc2e98d6b17330b86073ee325d7f5e
RezaRezaeian <rreza.rezaeian@gmail.com>
2023-11-16 15:12:29 -0500

    set click event on any button for show connected component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { style } from '@angular/animations';
 import {
   Component,
-  ElementRef,
-  Renderer2,
   ComponentFactoryResolver,
   ViewChild,
   ViewContainerRef,
+  ComponentRef,
+  OnDestroy,
+  ElementRef,
+  Renderer2,
 } from '@angular/core';
 import { LoginComponent } from './login/login.component';
+import { ListTableComponent } from './list-table/list-table.component';
+import { MapComponent } from './map/map.component';
+import { StartComponent } from './start/start.component';
+import { ImageComponent } from './image/image.component';
+import { InvestmentOpportunitiesComponent } from './investment-opportunities/investment-opportunities.component';
+import { CapacityComponent } from './capacity/capacity.component';
 
 @Component({
   selector: 'app-root',
@@ -19,9 +27,21 @@ export class AppComponent {
 
   @ViewChild('dynamicComponentContainer', {
     read: ViewContainerRef,
-    static: false,
+    static: true,
   })
   dynamicComponentContainer!: ViewContainerRef;
+
+  componentMap: { [key: string]: any } = {
+    LoginComponent: LoginComponent,
+    ListTableComponent: ListTableComponent,
+    MapComponent: MapComponent,
+    StartComponent: StartComponent,
+    ImageComponent: ImageComponent,
+    InvestmentOpportunitiesComponent: InvestmentOpportunitiesComponent,
+    CapacityComponent: CapacityComponent,
+  };
+
+  componentRef!: ComponentRef<any>;
 
   constructor(
     private elementRef: ElementRef,
@@ -29,16 +49,51 @@ export class AppComponent {
     private componentFactoryResolver: ComponentFactoryResolver
   ) {}
 
-  addComponent() {
+  // addComponent() {
+  //   const componentFactory =
+  //     this.componentFactoryResolver.resolveComponentFactory(LoginComponent);
+  //   const componentRef =
+  //     this.dynamicComponentContainer.createComponent(componentFactory);
+  // }
+
+  // replaceComponent() {
+  //   // Destroy the existing component
+  //   if (this.componentRef) {
+  //     this.componentRef.destroy();
+  //   }
+
+  //   // Resolve the factory for the new component
+  //   const componentFactory =
+  //     this.componentFactoryResolver.resolveComponentFactory(ListTableComponent);
+
+  //   // Create the new component
+  //   this.componentRef =
+  //     this.dynamicComponentContainer.createComponent(componentFactory);
+  // }
+
+  // ngOnDestroy(): void {
+  //   // Destroy the component reference when the component is destroyed
+  //   if (this.componentRef) {
+  //     this.componentRef.destroy();
+  //   }
+  // }
+
+  openComponent(componentKey: string) {
+    console.log(componentKey);
+
+    // Destroy the existing component
+    if (this.componentRef) {
+      this.componentRef.destroy();
+    }
+
+    // Resolve the factory for the selected component
     const componentFactory =
-      this.componentFactoryResolver.resolveComponentFactory(LoginComponent);
-    const componentRef =
+      this.componentFactoryResolver.resolveComponentFactory(
+        this.componentMap[componentKey]
+      );
+
+    // Create the new component
+    this.componentRef =
       this.dynamicComponentContainer.createComponent(componentFactory);
-  }
-
-  isCorrect: boolean = false;
-
-  n(value: Element) {
-    this.renderer.setStyle(value, 'color', '#ffe000');
   }
 }
