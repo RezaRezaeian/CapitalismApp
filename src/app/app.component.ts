7fcf27e7a531d0e2979c00d8528a25772aae85c0
RezaRezaeian <rreza.rezaeian@gmail.com>
2023-11-16 04:20:07 -0500

    add click event for add login component to app when user click on login button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,13 @@
-import { Component } from '@angular/core';
+import { style } from '@angular/animations';
+import {
+  Component,
+  ElementRef,
+  Renderer2,
+  ComponentFactoryResolver,
+  ViewChild,
+  ViewContainerRef,
+} from '@angular/core';
+import { LoginComponent } from './login/login.component';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +17,28 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'CapitalismApp';
 
-  salam() {
-    console.log('salam');
+  @ViewChild('dynamicComponentContainer', {
+    read: ViewContainerRef,
+    static: false,
+  })
+  dynamicComponentContainer!: ViewContainerRef;
+
+  constructor(
+    private elementRef: ElementRef,
+    private renderer: Renderer2,
+    private componentFactoryResolver: ComponentFactoryResolver
+  ) {}
+
+  addComponent() {
+    const componentFactory =
+      this.componentFactoryResolver.resolveComponentFactory(LoginComponent);
+    const componentRef =
+      this.dynamicComponentContainer.createComponent(componentFactory);
+  }
+
+  isCorrect: boolean = false;
+
+  n(value: Element) {
+    this.renderer.setStyle(value, 'color', '#ffe000');
   }
 }
